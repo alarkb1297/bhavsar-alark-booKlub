a8a474509c44a8379789d05b9b03ed117e428764
Alark Bhavsar <alarkb1297@gmail.com>
2017-08-14 18:55:05 -0400

    init commit

--- test/app.js
+++ test/app.js
@@ -10,7 +10,7 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds117899.mlab.com:17899/heroku_x999bdqj'; // user yours
+    connectionString += '@ds133932.mlab.com:33932/heroku_q8l74q0j'; // user yours
 }
 // Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
 // above with your own URL given to you by mLab
